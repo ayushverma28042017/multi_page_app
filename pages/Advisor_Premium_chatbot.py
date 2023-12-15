bde1fcd2c421f6a44d6a38fecbabf49bdf6da38d
Ayush Kumar <ayush@singlife.com>
2023-12-15 15:40:32 +0800

    updatedd

--- pages/Advisor_Premium_chatbot.py
+++ pages/Advisor_Premium_chatbot.py
@@ -17,7 +17,7 @@ headers = {
 }
 with st.form(key = 'userdata'):
     # st.write('data')
-    prompt = st.text_area("Enter Q&A  and get the playback (Summary):", key='prompt')
+    prompt = st.text_area("Enter Q&A  :", key='prompt')
     # input_data = "Create summary in 300 words in very simple english language without any grammar mistake ,simple sentence ,active voice and use more we and you and keep usage of promoun for below conversation between Financial Advisor and Customer :\n\nConversation:"+prompt
     submit_form = st.form_submit_button(label="submit", help="Click to submit")
     if submit_form:
@@ -25,7 +25,12 @@ with st.form(key = 'userdata'):
     {
       "role": "system",
       "content": "you are a Financial advisor and you need to create a summary for the user for all the questions and answers given by the user You need to follow the below instructions: \nuse short sentences- \nActive voice\nuse more we and write in a positive\nAvoid hidden verbs by being more direct and to the point"
-    }
+    },
+    ,
+    {
+      "role": "user",
+      "content": prompt
+    
   ],
   "temperature": 0,
   "top_p": 1,
