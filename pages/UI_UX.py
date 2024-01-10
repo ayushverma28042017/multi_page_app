a50d2e43bd15b5f8e599a9951c3e8375ea295fd4
Ayush Kumar <ayush@singlife.com>
2024-01-10 16:46:03 +0800

    update UIUX

--- pages/UI_UX.py
+++ pages/UI_UX.py
@@ -8,6 +8,7 @@ import sqlite3
 load_dotenv(".streamlit/secrets.toml")
 url=os.environ["AZURE_OPENAI_ENDPOINT_CHAT"]
 api_key=os.environ["AZURE_OPENAI_API_KEY"]
+
  
 headers = {
 
@@ -16,24 +17,27 @@ headers = {
     "Content-Type": "application/json"
 }
 
-def insert_data_in_db(data):
-    cxn = sqlite3.connect("UI_UX"+'.db')
-    sql_createtable = """ CREATE TABLE IF NOT EXISTS UI_UX(
+def initdb():
+       cxn = sqlite3.connect("UIUX"+'.db')
+       sql_createtable = """ CREATE TABLE IF NOT EXISTS UIUX(
                                         name text NOT NULL
                                     );
                                       """
-    
-    c = cxn.cursor()
-    c.execute(sql_createtable)
+       c = cxn.cursor()
+       c.execute(sql_createtable)
+
+def insert_data_in_db(data):
+ 
     #INSERT INTO artists (name) VALUES('Bud Powell');
     # c.execute("insert into contacts (name) values"), (data)")
-    c.execute("insert into UI_UX(name) values(?)",(data,))
+    c.execute("insert into UIUX(name) values(?)",(data,))
     cxn.commit()
     cxn.close() 
 
 
 with st.form(key = 'userdata'):
     # st.write('data')
+    initdb()
     prompt = st.text_area("Enter your input  :", key='prompt')
     # input_data = "Create summary in 300 words in very simple english language without any grammar mistake ,simple sentence ,active voice and use more we and you and keep usage of promoun for below conversation between Financial Advisor and Customer :\n\nConversation:"+prompt
     submit_form = st.form_submit_button(label="submit", help="Click to submit")
@@ -66,7 +70,10 @@ with st.form(key = 'userdata'):
              myfile.write(prompt)
              myfile.write("\n\n")
             #  myfile.write()
-             insert_data_in_db(response.json()["choices"][0]["message"]["content"])
+             res =response.json()["choices"][0]["message"]["content"]
+             resformatted= f"{res}"
+             
+             insert_data_in_db(resformatted)
              myfile.write("\n\n\n\n\n")
       else:
         
@@ -76,10 +83,10 @@ with st.form(key = 'userdata'):
 with st.form(key = 'history'): 
      view_history = st.form_submit_button(label="view", help="Click to view")
      if view_history:
-        cxn = sqlite3.connect("UI_UX"+'.db')
+        cxn = sqlite3.connect("UIUX"+'.db')
    
         c = cxn.cursor()
-        c.execute("select * from UI_UX" ) 
+        c.execute("select * from UIUX" ) 
         found_records = c.fetchall();
         for record in found_records:
             st.write(record)
